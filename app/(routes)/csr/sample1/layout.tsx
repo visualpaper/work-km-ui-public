8bef3a283a4f5e8d19c6a1abee3138cb3be5f8a0
umejima <kazuya.umejima@gmail.com>
2024-04-21 00:39:00 +0900

    main context をまず作る

---
+++ app/(routes)/csr/sample1/layout.tsx
@@ -0,0 +1,7 @@
+export default function CsrSample1Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return <>{children}</>
+}
---
+++ app/(routes)/csr/sample1/page.tsx
@@ -0,0 +1,5 @@
+"use client"
+
+export default function CsrSample1() {
+  return <>aaa</>
+}
