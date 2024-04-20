58f7926819efc0c43e3efc4c2215dcf7b081211d
visualpaper <kazuya.umejima@gmail.com>
2024-04-21 05:10:07 +0900

    見直し (#7)

--- app/_lib/messages.ts
+++ app/_lib/messages.ts
@@ -1,11 +1,13 @@
 export const ERROR_CODE = {
   CONFLICT: '',
   SERVER_UNEXPECTED: 'BASE-0000',
+  SAMPLE_ERROR: 'BASE-0001',
   CLIENT_ERROR: 'CLIENT_ERROR', // これは内部用
 }
 
 export const ERROR_MESSAGES = {
   [ERROR_CODE.CONFLICT]: 'Conflict',
   [ERROR_CODE.SERVER_UNEXPECTED]: 'Server unexpected',
+  [ERROR_CODE.SAMPLE_ERROR]: 'Sample Error',
   [ERROR_CODE.CLIENT_ERROR]: 'Client Erorr',
 }
